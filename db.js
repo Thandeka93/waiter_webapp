7d4ab3752faa0403b78c96104451468ec1401b7e
Thandeka93 <noluthandombali4@gmail.com>
2023-10-03 14:43:51 +0200

    workflow

--- db.js
+++ db.js
@@ -1,19 +1,23 @@
 import pgPromise from 'pg-promise';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const pgp = pgPromise();
 
-// for SSL connection
+// For SSL connection
 let useSSL = false;
 let local = process.env.LOCAL || false;
+
 if (process.env.DATABASE_URL && !local) {
     useSSL = true;
 }
-// db connection 
-const connectionString = process.env.PGDATABASE_URL || 'postgres://ersfpvqe:bYZyNT95SJyVuqA45h3TYcLIJb6bWynP@dumbo.db.elephantsql.com/ersfpvqe'
+
+// DB connection
+const connectionString = process.env.PGDATABASE_URL || 'postgres://ersfpvqe:bYZyNT95SJyVuqA45h3TYcLIJb6bWynP@dumbo.db.elephantsql.com/ersfpvqe';
 
 const db = pgp(connectionString);
-db.connect();
 
-export default db
+// You don't need to call db.connect() explicitly in newer versions of pg-promise
+
+export default db;
