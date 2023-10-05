a8abd095ed1b6542993821d6fac2b26ebbe1bd64
Thandeka93 <noluthandombali4@gmail.com>
2023-10-05 09:07:09 +0200

    route

--- test/waiter.test.js
+++ test/waiter.test.js
@@ -1,6 +1,6 @@
 import assert from "assert";
 import waiterService from "../services/query.js";
-import pgPromise from "pg-promise";
+// import pgPromise from "pg-promise";
 
 const mockDbConnection = {
     none: async () => { },
@@ -9,10 +9,10 @@ const mockDbConnection = {
     manyOrNone: async () => { },
 };
 
-const connectionString = process.env.PGDATABASE_URL ||
-    'postgres://ersfpvqe:bYZyNT95SJyVuqA45h3TYcLIJb6bWynP@dumbo.db.elephantsql.com/ersfpvqe';
-const pgp = pgPromise();
-const db = pgp(connectionString);
+// const connectionString = process.env.PGDATABASE_URL ||
+//     'postgres://ersfpvqe:bYZyNT95SJyVuqA45h3TYcLIJb6bWynP@dumbo.db.elephantsql.com/ersfpvqe';
+// const pgp = pgPromise();
+// const db = pgp(connectionString);
 
 describe('waiterService', () => {
     // Initialize the waiter service with the mock database connection
@@ -20,7 +20,7 @@ describe('waiterService', () => {
 
     describe('waiters', () => {
         it('should insert a new waiter and their availability', async () => {
-            await service.waiters('John', ['Monday', 'Wednesday']);
+            await service.updateWaiterAvailability('John', ['Monday', 'Wednesday']);
             assert.notEqual('John', 2);
         });
     });
