7b8993c605c231cdd31010f4f0cc57b615719762
Thandeka93 <tgamede93@gmail.com>
2023-11-03 09:26:05 +0200

    helpers

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 import express from 'express';
 import exphbs from 'express-handlebars';
-import {engine} from 'express-handlebars'
+import { engine } from 'express-handlebars'
 import bodyParser from 'body-parser';
 import flash from 'express-flash';
 import pgPromise from 'pg-promise';
 import session from 'express-session';
 import appRoutes from './routes/waiterRoutes.js';
 import createDatabaseQueries from './services/query.js';
-// import db from './db.js';
 
 const app = express();
 const hbs = exphbs.create();
 
-const connectionString =
-  process.env.PGDATABASE_URL || 'postgres://ersfpvqe:bYZyNT95SJyVuqA45h3TYcLIJb6bWynP@dumbo.db.elephantsql.com/ersfpvqe'
-  
+const connectionString = process.env.PGDATABASE_URL ||
+ 'postgres://ersfpvqe:bYZyNT95SJyVuqA45h3TYcLIJb6bWynP@dumbo.db.elephantsql.com/ersfpvqe'
 
 const pgp = pgPromise();
 const db = pgp(connectionString);
@@ -27,47 +25,42 @@ const waiterRoute = appRoutes(waiterdb)
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
-    secret: 'secret-key',
-    resave: false,
-    saveUninitialized: true
-  }));
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(flash());
 
+//handlebars helpers
 
+hbs.handlebars.registerHelper('includes', function (arr, item) {
+  // Check if arr is defined and is an array
+  if (Array.isArray(arr)) {
+    // Map the array of objects to an array of days
+    const days = arr.map(a => a.day);
+    // Check if the item is included in the array of days
+    return days.includes(item);
+  } else {
+    // Handle the case where arr is not an array
+    return false;
+  }
+});
 
-//handlebars engine
-
-hbs.handlebars.registerHelper('includes', function(arr, item) {
-    // Check if arr is defined and is an array
-    if (Array.isArray(arr)) {
-      // Map the array of objects to an array of days
-      const days = arr.map(a => a.day);
-      // Check if the item is included in the array of days
-      return days.includes(item);
-    } else {
-      // Handle the case where arr is not an array
-      return false;
-    }
-  });
-
-  hbs.handlebars.registerHelper('getStatus', function(count) {
-    if (count === 1) {
-      return 'red';
-    } else if (count === 2) {
-      return 'orange';
-    } else if (count === 3) {
-      return 'green';
-    } else {
-      return 'grey';
-    }
-  });
-  
-
+hbs.handlebars.registerHelper('getStatus', function (count) {
+  if (count === 1) {
+    return 'red';
+  } else if (count === 2) {
+    return 'orange';
+  } else if (count === 3) {
+    return 'green';
+  } else {
+    return 'grey';
+  }
+});
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
-//public static
 app.use(express.static('public'));
 
 app.get('/', waiterRoute.renderIndex)
@@ -78,13 +71,7 @@ app.get('/waiters/:username', waiterRoute.renderWaiters)
 app.post('/reset', waiterRoute.resetSchedule)
 
 
-      
-
-
-
-//local host 
 const PORT = process.env.PORT || 3006
-
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
